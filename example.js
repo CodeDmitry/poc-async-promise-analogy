8bd55d909244ddb71d5695fb3d9fcedb2e2d2f82
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-03 22:03:06 -0400

    Update example.js

--- example.js
+++ example.js
@@ -4,14 +4,17 @@ async function A1() {
     console.log('A1 returning');
     return 3;
 }
+
 async function B1() {
     console.log('B1 returning');
     return 4;
 }
+
 async function C1() {
     console.log('C1 returning');
     return 5;
 }
+
 (async () => {
     // the below are executed in order.
     var a = await A1();
@@ -26,10 +29,12 @@ function A2() {
     console.log('A2 returning');
     return new Promise(accept => accept(3));
 }
+
 function B2() {
     console.log('B2 returning');
     return new Promise(accept => accept(4));    
 }
+
 function C2() {
     console.log('C2 returning');
     return new Promise(accept => accept(5));
